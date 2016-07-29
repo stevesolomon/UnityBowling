4d01e276e86d80848004df9676f0301bc62688f0
stevesolomon <steven@steven-solomon.com>
2016-07-28 21:20:52 -0700

    Added ball and initial script

---
+++ Assets/Scripts/Ball.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class Ball : MonoBehaviour
+{
+    public float launchSpeed = 100f;
+
+    private Rigidbody myRigidbody;
+
+	// Use this for initialization
+	void Start ()
+    {
+        this.myRigidbody = GetComponent<Rigidbody>();
+
+        this.myRigidbody.velocity = new Vector3(0f, 0f, launchSpeed);
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+	
+
+	}
+}
