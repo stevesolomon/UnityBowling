0527be4a975f8d836e2807467425c3244cc948f1
stevesolomon <steven@steven-solomon.com>
2016-07-31 20:38:45 -0700

    Added testing for pins left standing with UI display

---
+++ Assets/Scripts/Pin.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+public class Pin : MonoBehaviour
+{
+    public float standingThresholdDegrees = 5f;
+
+	// Use this for initialization
+	void Start ()
+    {
+	
+	}
+
+    public bool IsStanding()
+    {
+        Vector3 rotation = transform.rotation.eulerAngles;
+
+        float xTilt = Mathf.Abs(rotation.x);
+        float zTilt = Mathf.Abs(rotation.z);
+
+        if ((xTilt < standingThresholdDegrees || xTilt > 360 - standingThresholdDegrees) && 
+            (zTilt < standingThresholdDegrees || zTilt > 360 - standingThresholdDegrees))
+        {
+            return true;
+        }
+
+        return false;
+    }
+}
---
+++ Assets/Scripts/PinSetter.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class PinSetter : MonoBehaviour
+{
+    public Text pinText;
+
+    private Pin[] pins;
+
+    private string pinTextFormat = "{0} Pins Remaining";
+
+	// Use this for initialization
+	void Start ()
+    {
+        pins = GameObject.FindObjectsOfType<Pin>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        int pinsStanding = PinsStanding();
+
+        pinText.text = string.Format(pinTextFormat, pinsStanding);
+	}
+
+    private int PinsStanding()
+    {
+        int pinsStanding = 0;
+
+        foreach (var pin in this.pins)
+        {
+            if (pin.IsStanding())
+            {
+                pinsStanding++;
+            }
+        }
+
+        return pinsStanding;
+    }
+}
