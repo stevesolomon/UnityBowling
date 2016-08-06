59c28fd3c6342f6eff023422019c81917ad95b30
stevesolomon <steven@steven-solomon.com>
2016-08-05 20:58:10 -0700

    Added initial ActionController

---
+++ Assets/Scripts/ActionController.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public enum ActionResponse
+{
+    Tidy,
+    Reset,
+    EndTurn,
+    EndGame
+}
+
+public class ActionController
+{
+	public ActionResponse Bowl(int pinsDropped)
+    {
+        if (pinsDropped < 0 || pinsDropped > 10)
+        {
+            throw new ArgumentException("pinsDropped must be between 0 and 10.");
+        }
+
+        if (pinsDropped == 10)
+        {
+            return ActionResponse.EndTurn;
+        }
+
+        return ActionResponse.Tidy;
+    }
+}
