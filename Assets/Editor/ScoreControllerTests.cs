c7db390855a22ade83f03156bf730e8bedfa1783
stevesolomon <steven@steven-solomon.com>
2016-08-14 20:48:39 -0700

    Completed ScoreController and associated unit tests

--- Assets/Editor/ScoreControllerTests.cs
+++ Assets/Editor/ScoreControllerTests.cs
@@ -1,4 +1,5 @@
 ﻿using NUnit.Framework;
+using System.Linq;
 
 namespace Assets.Editor
 {
@@ -10,6 +11,204 @@ namespace Assets.Editor
         public void Setup()
         {
             this.scoreController = new ScoreController();
+        }
+
+        [Test]
+        public void FirstFrameTwoRollsScore()
+        {
+            int[] rolls = { 2, 3 };
+            int[] frames = { 5 };
+
+            Assert.AreEqual(frames.ToList(), this.scoreController.FrameScores(rolls));
+        }
+
+        [Test]
+        public void FirstThreeRollsScore()
+        {
+            int[] rolls = { 2, 3, 4 };
+            int[] frames = { 5 };
+
+            Assert.AreEqual(frames.ToList(), this.scoreController.FrameScores(rolls));
+        }
+
+        [Test]
+        public void FirstTwoFramesScore()
+        {
+            int[] rolls = { 2, 3, 4, 5 };
+            int[] frames = { 5, 9 };
+
+            Assert.AreEqual(frames.ToList(), this.scoreController.FrameScores(rolls));
+        }
+
+        [Test]
+        public void FirstFiveRollsScore()
+        {
+            int[] rolls = { 2, 3, 4, 5, 6 };
+            int[] frames = { 5, 9 };
+
+            Assert.AreEqual(frames.ToList(), this.scoreController.FrameScores(rolls));
+        }
+
+        [Test]
+        public void FirstThreeFramesScore()
+        {
+            int[] rolls = { 2, 3, 4, 5, 6, 1 };
+            int[] frames = { 5, 9, 7 };
+
+            Assert.AreEqual(frames.ToList(), this.scoreController.FrameScores(rolls));
+        }
+
+        [Test]
+        public void FirstSevenRollsScore()
+        {
+            int[] rolls = { 2, 3, 4, 5, 6, 1, 2 };
+            int[] frames = { 5, 9, 7 };
+
+            Assert.AreEqual(frames.ToList(), this.scoreController.FrameScores(rolls));
+        }
+
+        [Test]
+        public void BowlStrikeThenOneScore()
+        {
+            int[] rolls = {10, 1};
+            int[] frames = { };
+
+            Assert.AreEqual(frames.ToList(), this.scoreController.FrameScores(rolls));
+        }
+
+        [Test]
+        public void BowlSpareScore()
+        {
+            int[] rolls = { 1, 9 };
+            int[] frames = { };
+
+            Assert.AreEqual(frames.ToList(), this.scoreController.FrameScores(rolls));
+        }
+
+        [Test]
+        public void BowlTwoFramesThenStrike()
+        {
+            int[] rolls = { 1, 2, 3, 4, 5, 5 };
+            int[] frames = { 3, 7 };
+
+            Assert.AreEqual(frames.ToList(), this.scoreController.FrameScores(rolls));
+        }
+
+        [Test]
+        public void HandleSpareBonus()
+        {
+            int[] rolls = { 1, 2, 3, 5, 5, 5, 3, 3};
+            int[] frames = { 3, 8, 13, 6 };
+
+            Assert.AreEqual(frames.ToList(), this.scoreController.FrameScores(rolls));
+        }
+
+        [Test]
+        public void HandleTwoSpareBonuses()
+        {
+            int[] rolls = { 1, 2, 3, 5, 5, 5, 3, 3, 7, 1, 9, 1, 6 };
+            int[] frames = { 3, 8, 13, 6, 8, 16 };
+
+            Assert.AreEqual(frames.ToList(), this.scoreController.FrameScores(rolls));
+        }
+
+        [Test]
+        public void HandleStrikeBonus()
+        {
+            int[] rolls = { 10, 3, 4 };
+            int[] frames = { 17, 7 };
+
+            Assert.AreEqual(frames.ToList(), this.scoreController.FrameScores(rolls));
+        }
+
+        [Test]
+        public void HandleStrikeBonusLaterFrames()
+        {
+            int[] rolls = { 1, 2, 3, 4, 5, 4, 3, 2, 10, 1, 3, 3, 4 };
+            int[] frames = { 3, 7, 9, 5, 14, 4, 7 };
+
+            Assert.AreEqual(frames.ToList(), this.scoreController.FrameScores(rolls));
+        }
+
+        [Test]
+        public void HandleMultipleConsecutiveStrikes()
+        {
+            int[] rolls = { 10, 10, 2, 3 };
+            int[] frames = { 22, 15, 5 };
+
+            Assert.AreEqual(frames.ToList(), this.scoreController.FrameScores(rolls));
+        }
+
+        [Test]
+        public void HandleMultipleConsecutiveStrikesWithAFrameSeparationStrike()
+        {
+            int[] rolls = { 10, 10, 2, 3, 10, 5, 3 };
+            int[] frames = { 22, 15, 5, 18, 8 };
+
+            Assert.AreEqual(frames.ToList(), this.scoreController.FrameScores(rolls));
+        }
+
+        [Test]
+        public void HandleGutterBallGame()
+        {
+            int[] rolls = { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+            int[] frames = { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+
+            Assert.AreEqual(frames.ToList(), this.scoreController.CumulativeScores(rolls));
+        }
+
+        [Test]
+        public void HandleSpareBonusFirstFrame()
+        {
+            int[] rolls = { 5, 5, 3 };
+            int[] frames = { 13 };
+
+            Assert.AreEqual(frames.ToList(), this.scoreController.CumulativeScores(rolls));
+        }
+
+        [Test]
+        public void HandleSpareInLastFrame()
+        {
+            int[] rolls = { 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 9, 7 };
+            int[] frames = { 2, 4, 6, 8, 10, 12, 14, 16, 18, 35 };
+
+            Assert.AreEqual(frames.ToList(), this.scoreController.CumulativeScores(rolls));
+        }
+
+        [Test]
+        public void HandleAllStrikesGame()
+        {
+            int[] rolls = { 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10 };
+            int[] frames = { 30, 60, 90, 120, 150, 180, 210, 240, 270, 300 };
+
+            Assert.AreEqual(frames.ToList(), this.scoreController.CumulativeScores(rolls));
+        }
+
+        [Test]
+        public void HandleStrikeInLastFrame()
+        {
+            int[] rolls = { 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 10, 2, 3 };
+            int[] frames = { 2, 4, 6, 8, 10, 12, 14, 16, 18, 33 };
+
+            Assert.AreEqual(frames.ToList(), this.scoreController.CumulativeScores(rolls));
+        }
+
+        [Test]
+        public void HandleRealWorldGameScore()
+        {
+            int[] rolls = { 10, 9, 1, 9, 1, 9, 1, 9, 1, 7, 0, 9, 0, 10, 8, 2, 8, 2, 10 };
+            int[] frames = { 20, 39, 58, 77, 94, 101, 110, 130, 148, 168 };
+
+            Assert.AreEqual(frames.ToList(), this.scoreController.CumulativeScores(rolls));
+        }
+
+        [Test]
+        public void HandleRealWorldGameScore2()
+        {
+            int[] rolls = { 8, 2, 8, 1, 9, 1, 7, 1, 8, 2, 9, 1, 9, 1, 10, 10, 7, 1 };
+            int[] frames = { 18, 27, 44, 52, 71, 90, 110, 137, 155, 163 };
+
+            Assert.AreEqual(frames.ToList(), this.scoreController.CumulativeScores(rolls));
         }
     }
 }
--- Assets/Scripts/ScoreController.cs
+++ Assets/Scripts/ScoreController.cs
@@ -1,16 +1,75 @@
 ﻿using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 
 public class ScoreController
 {
-    public static IList<int> FrameScores (IEnumerable<int> rolls)
+    /// <summary>
+    /// Returns a list of individual frame scores, not cumulative scores.
+    /// </summary>
+    /// <param name="rolls"></param>
+    /// <returns></returns>
+    public IList<int> FrameScores (IEnumerable<int> rolls)
     {
         var frameScores = new List<int>();
 
+        for (int i = 1; i < rolls.Count(); i += 2)
+        {
+            // Prevent an 11th frame score due to strikes on the final frame.
+            if (frameScores.Count() == 10)
+            {
+                break;
+            }
 
+            int lastRoll = rolls.ElementAt(i - 1);
+            int currRoll = rolls.ElementAt(i);
+
+            // Standard frame
+            if (lastRoll + currRoll < 10)
+            {
+                frameScores.Add(lastRoll + currRoll);
+            }
+
+            if (rolls.Count() - i <= 1)
+            {
+                break;
+            }
+
+            //Strike and bonus
+            if (lastRoll == 10)
+            {
+                i--; //Strike frame has a single bowl.
+                frameScores.Add(10 + rolls.ElementAt(i + 1) + rolls.ElementAt(i + 2));
+            }
+            //Spare and bonus
+            else if (lastRoll + currRoll == 10)
+            {
+                frameScores.Add(10 + rolls.ElementAt(i + 1));
+            }
+        }
 
         return frameScores;
     }
     
+    /// <summary>
+    /// Returns a list of cumulative scores like a traditional bowling score card.
+    /// </summary>
+    /// <param name="rolls"></param>
+    /// <returns></returns>
+    public IList<int> CumulativeScores(IEnumerable<int> rolls)
+    {
+        List<int> cumulativeScores = new List<int>();
+
+        var frameScores = this.FrameScores(rolls);
+        int runningTotal = 0;
+
+        foreach (var frameScore in frameScores)
+        {
+            runningTotal += frameScore;
+            cumulativeScores.Add(runningTotal);
+        }
+
+        return cumulativeScores;
+    }
 }
